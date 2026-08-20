47272d6264b673d9e5cda5fa4740a1ac191fc36f
Daniel Biondi <biondidaniel94@gmail.com>
2026-08-20 03:22:11 -0300

    Upd - Criação da estrutura MVC e as pastas

---
+++ app/Http/Controllers/PeiMusicalController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PeiMusical;
+use Illuminate\Http\Request;
+
+class PeiMusicalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PeiMusical $peiMusical)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PeiMusical $peiMusical)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PeiMusical $peiMusical)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PeiMusical $peiMusical)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PlanoDeAulaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PlanoDeAula;
+use Illuminate\Http\Request;
+
+class PlanoDeAulaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PlanoDeAula $planoDeAula)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PlanoDeAula $planoDeAula)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PlanoDeAula $planoDeAula)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PlanoDeAula $planoDeAula)
+    {
+        //
+    }
+}
