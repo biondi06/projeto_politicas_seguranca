51960b6ac56a4dc553891f836feeab9015a8e50a
Daniel Biondi <biondidaniel94@gmail.com>
2026-09-10 19:02:51 -0300

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
-
         $middleware->alias([
             'two_factor_required' => \App\Http\Middleware\EnsureTwoFactorIsEnabled::class,
         ]);
